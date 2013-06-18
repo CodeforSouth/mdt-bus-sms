8f01e9bdd3cbb5f5c76dbeed0052d9fcbd0637b9
Adrian Cardenas <arcardenas@gmail.com>
2013-06-17 23:27:54 -0500

    Added fetch based on bus & stop ID. Added logging for the SMS messages

--- src/SmsBus/Db/StopTimesTable.php
+++ src/SmsBus/Db/StopTimesTable.php
@@ -14,6 +14,21 @@ class StopTimesTable extends AbstractTable {
 	public function fetch($id) {
 		
 	}
+	
+	public function fetchByBusStop($stop = 0, $bus = 0) {
+		if(intval($stop) === 0 || intval($bus) === 0) {
+			return false;
+		}
+		
+		$sql = "SELECT `stop_times`.*, `routes`.* FROM `stop_times` LEFT JOIN `trips` ON `stop_times`.`trip_id` = `trips`.`trip_id` LEFT JOIN `routes` ON `trips`.`route_id` = `routes`.`route_id` WHERE `stop_times`.`stop_id` = :stop_id AND `routes`.`route_short_name` = :bus_id AND `stop_times`.`arrival_time` > CURTIME() LIMIT 0, 3";
+		$stmt = $this->dbConn->prepare($sql);
+		$result = $stmt->execute(array(":stop_id" => $stop, ":bus_id" => $bus));
+		if(!$result) {
+			//Should do error logging here
+			return false;
+		}
+		return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+	}
 	public function update($data = array()) {
 		
 	}
