6417ac5fd402af1886d275624139611d0944c71a
Adrian Cardenas <arcardenas@gmail.com>
2013-08-12 21:24:26 -0400

    Added Fare attributes table class for broward data

---
+++ src/SmsBus/Db/FareAttributesTable.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: aramonc
+ * Date: 8/12/13
+ * Time: 7:26 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace SmsBus\Db;
+
+
+class FareAttributesTable {
+
+}
