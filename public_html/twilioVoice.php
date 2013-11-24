6c84a08547f96ddd78e8fb4d0304a3a27b679c2c
Adrian Cardenas <arcardenas@gmail.com>
2013-11-24 13:32:08 -0500

    Changed the message to the description of the service.

--- public_html/twilioVoice.php
+++ public_html/twilioVoice.php
@@ -7,5 +7,5 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 ?>
 
 <Response>
-<Say voice="woman">Hello <?php echo $name ?>, thank you for calling the text my bus, help line. I will be happy to assist you!.</Say>
+<Say voice="woman">Hello <?php echo $name; ?>, Text My Bus MIA is an SMS system that enables the user to text the ID number of his or her bus stop and receive a message with upcoming bus times for that stop. The goal is to provide easy access to updated bus schedules for low-income individuals and other Miami-Dade County residents and visitors who rely on public transportation for commuting and getting around the county. For more information visit http://www.codeformiami.org.</Say>
 </Response>
