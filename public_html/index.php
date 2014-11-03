ae2effd5f702aa311193fa063f95109d95dfe272
Adrian Cardenas <arcardenas@gmail.com>
2014-11-03 00:12:51 -0500

    mdt-bus-sms-38 setting index.php to be a silex app

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,15 @@
 <?php
 
-header('Location: http://miamiwiki.org/SMSBus_Project');
+// SET DEFAULT TIME ZONE IN CASE NOT SET IN INI
+date_default_timezone_set('America/New_York');
 
-?>
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+// REDIRECT GET REQUESTS TO THE MIAMI WIKI
+$app->get('/', function() use ($app) {
+    return $app->redirect('http://miamiwiki.org/SMSBus_Project');
+});
+
+$app->run();
